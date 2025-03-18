f26105cff65ce3c9f0a235f30d3feb1e87f84b28
xiting.zhao <tina102023@163.com>
2025-03-18 16:57:26 +0100

    add

---
+++ 爱心代码.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+int main(int argc,char *argv[])
+{
+	float x,y,a;
+	for(y=1.5;y>-1.5;y-=0.1)
+	{for(x=-1.5;x<1.5;x+=0.05)
+	{a=x*x+y*y-1;
+	putchar(a*a*a-x*x*y*y*y<=0.0?'*':' ');
+	}
+	system("color 0c");
+	putchar('\n');
+	}
+	printf("I wonder know \n");
+	printf("Will you miss me?\n");
+	return 0;
+}
